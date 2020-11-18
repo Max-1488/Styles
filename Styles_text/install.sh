2473afc82757a47a4bee7cb5e54dc4d79fdadd41
Max-1488 <64449711+Max-1488@users.noreply.github.com>
2020-11-17 23:01:01 -0600

    Update install.sh

--- Styles_text/install.sh
+++ Styles_text/install.sh
@@ -1,4 +1,5 @@
 pip install render
+##Descargaremos esta herramienta por motivos de error en los modulos de python3
 git clone https://github.com/pwaller/pyfiglet.git
 pip install pyfiglet
 pip install figlet
